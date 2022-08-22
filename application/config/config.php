383af167d39f966ccdc3735b936f50d701aa422d
Misrudin <37394664+misrudin@users.noreply.github.com>
2022-08-23 00:37:10 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-// $config['base_url'] = 'http://localhost/simoisen/';
+$config['base_url'] = 'https://monkfish-app-d53ds.ondigitalocean.app/';
 
 /*
 |--------------------------------------------------------------------------
